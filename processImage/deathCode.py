e4bfc38894ae88dd45fb9c6ba1d91cf608b67434
shavbeens <shaviyobm@outlook.com>
2019-11-24 13:52:42 -0500

    added cv2imageload for testing purposes

---
+++ processImage/deathCode.py
@@ -0,0 +1,32 @@
+import cv2
+import numpy as np
+import os
+
+# Playing video from file:
+cap = cv2.VideoCapture(0)
+
+try:
+    if not os.path.exists('data'):
+        os.makedirs('data')
+except OSError:
+    print ('Error: Creating directory of data')
+
+currentFrame = 0
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # Saves image of the current frame in jpg file
+    name = './data/frame' + str(currentFrame) + '.jpg'
+    print ('Creating...' + name)
+    cv2.imwrite(name, frame)
+
+    # To stop duplicate images
+    currentFrame += 1
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()q

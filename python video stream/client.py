0db09ad50b42f136676d679ad7d7929a63f847c9
BrianRPhilip <32573732+BrianRPhilip@users.noreply.github.com>
2020-01-11 12:52:57 -0500

    UDP connection for transferring images

---
+++ python video stream/client.py
@@ -0,0 +1,44 @@
+import io
+import socket
+import struct
+import time
+import picamera
+
+pi_socket = socket.socket()
+
+pi_socket.connect(('192.168.2.17', 8000))  # ADD Computer ip here HERE
+
+print('Enter quit to stop sending images to server: ')
+
+# Make a file-like object out of the connection. Object exposing a file-oriented API, in this case a buffer like object
+connection = pi_socket.makefile('wb')
+try:
+    #Camera setup
+    camera = picamera.PiCamera()
+    camera.vflip = True
+    camera.resolution = (500, 480)
+
+    # Note the start time and construct a stream to hold image data
+    # temporarily (we could write it directly to connection but in this
+    # case we want to find out the size of each capture first to keep
+    # our protocol simple)
+    start = time.time()
+    stream = io.BytesIO()
+    while input() != quit:
+        camera.capture(stream, 'jpeg')
+        # Write the length of the capture to the stream and flush to
+        # ensure it actually gets sent
+        connection.write(struct.pack('<L', stream.tell()))
+        connection.flush()
+        # sets steam's current position
+        stream.seek(0)
+        connection.write(stream.read())
+        stream.seek(0)
+        stream.truncate()
+        # need to optimize this sleep time depending on our algorithms efficiency
+        time.sleep(.5)
+    # Write a length of zero to the stream to signal we're done
+    connection.write(struct.pack('<L', 0))
+finally:
+    connection.close()
+    client_socket.close()
